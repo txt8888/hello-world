1fb88f2d529632e8d4a7df01ec6c0ea64c6b39ed
Timothy Phan <Timothy.Phan@Raytheon.com>
2018-05-10 09:11:58 -0500

    cleaned up, add bash to invoke package

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -3,9 +3,5 @@
 set -xeu
 
 cd source-code/
-# ./mvnw package
-# mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dhttp.proxyHost=proxy.ext.ray.com -Dhttp.proxyPort=80 -DproxySet=true -DproxyHost=proxy.ext.ray.com -DproxyPort=80 package
-
-
 
 mvn --settings ./settings.xml package
