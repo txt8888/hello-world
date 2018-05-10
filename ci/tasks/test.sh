70c32f4eb3039d85161642dac5b68f2724de35b9
Timothy Phan <Timothy.Phan@Raytheon.com>
2018-05-10 09:07:17 -0500

    added settings.xml

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -4,4 +4,6 @@ set -xeu
 
 cd source-code/
 #bash ./mvnw test
-mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dhttp.proxyHost=proxy.ext.ray.com -Dhttp.proxyPort=80 -DproxySet=true -DproxyHost=proxy.ext.ray.com -DproxyPort=80 test
+#mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dhttp.proxyHost=proxy.ext.ray.com -Dhttp.proxyPort=80 -DproxySet=true -DproxyHost=proxy.ext.ray.com -DproxyPort=80 test
+
+mvn --settings ./settings.xml test
